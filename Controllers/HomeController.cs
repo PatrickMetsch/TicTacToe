d13284e76bb1a89e1224769cbf5c55cbdae35413
Patrick Metsch <patrickmetsch@Patricks-MacBook-Pro.local>
2020-11-03 02:17:26 -0500

    TicTacToe

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Tic.Models;
+using Newtonsoft.Json;
+
+namespace Tic.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            State state = new State();
+            string stateJson = JsonConvert.SerializeObject(state);
+            HttpContext.Session.SetString("state", stateJson);
+            return View();
+        }
+
+        public IActionResult Update(string id)
+        {
+            string stateString = HttpContext.Session.GetString("state") ?? "";
+            State state = JsonConvert.DeserializeObject<State>(stateString);
+
+            state.GetType().GetProperty(id).SetValue(state, state.Character);
+
+            state.TurnNumber += 1;
+
+            string newCharacter = state.TurnNumber % 2 == 0 ? "X" : "O";
+            state.Character = newCharacter;
+
+            string newStateString = JsonConvert.SerializeObject(state);
+            HttpContext.Session.SetString("state", newStateString);
+
+            bool isWinner = false;
+
+            string tl = state.TopLeft;
+            string tm = state.TopMiddle;
+            string tr = state.TopRight;
+
+            string cl = state.CenterLeft;
+            string cm = state.CenterMiddle;
+            string cr = state.CenterRight;
+
+            string bl = state.BottomLeft;
+            string bm = state.BottomMiddle;
+            string br = state.BottomRight;
+
+            string[] topRow = { tl, tm, tr };
+            string[] middleRow = { cl, cm, cr };
+            string[] bottomRow = { bl, bm, br };
+
+            string[] leftCol = { tl, cl, bl };
+            string[] middleCol = { tm, cm, bm };
+            string[] rightCol = { tr, cr, br };
+
+            string[] lrDiag = { tl, cm, br };
+            string[] rlDiag = { tr, cm, bl };
+
+            string[] xWin = { "X", "X", "X" };
+            string[] oWin = { "O", "O", "O" };
+
+            if (
+                (Enumerable.SequenceEqual(topRow, xWin) || Enumerable.SequenceEqual(topRow, oWin)) ||
+                (Enumerable.SequenceEqual(middleRow, xWin) || Enumerable.SequenceEqual(middleRow, oWin)) ||
+                (Enumerable.SequenceEqual(bottomRow, xWin) || Enumerable.SequenceEqual(bottomRow, oWin)) ||
+                (Enumerable.SequenceEqual(leftCol, xWin) || Enumerable.SequenceEqual(leftCol, oWin)) ||
+                (Enumerable.SequenceEqual(middleCol, xWin) || Enumerable.SequenceEqual(middleCol, oWin)) ||
+                (Enumerable.SequenceEqual(rightCol, xWin) || Enumerable.SequenceEqual(rightCol, oWin)) ||
+                (Enumerable.SequenceEqual(lrDiag, xWin) || Enumerable.SequenceEqual(lrDiag, oWin)) ||
+                (Enumerable.SequenceEqual(rlDiag, xWin) || Enumerable.SequenceEqual(rlDiag, oWin))
+            )
+            {
+                isWinner = true;
+            }
+
+            if (isWinner == true)
+            {
+                string sString = HttpContext.Session.GetString("state") ?? "";
+                State s = JsonConvert.DeserializeObject<State>(sString);
+                s.TurnNumber += 1;
+
+                string newChar = s.TurnNumber % 2 == 0 ? "X" : "O";
+                s.Character = newChar;
+
+                string newSString = JsonConvert.SerializeObject(s);
+                HttpContext.Session.SetString("state", newSString);
+
+                return RedirectToAction("Winner");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult Winner()
+        {
+            return View();
+        }
+    }   
+}
---
+++ Models/State.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Tic.Models
+{
+    public class State
+    {
+        public int TurnNumber { get; set; }
+
+        public string Character { get; set; }
+
+        public string TopLeft { get; set; }
+        public string TopMiddle { get; set; }
+        public string TopRight { get; set; }
+
+        public string CenterLeft { get; set; }
+        public string CenterMiddle { get; set; }
+        public string CenterRight { get; set; }
+
+        public string BottomLeft { get; set; }
+        public string BottomMiddle { get; set; }
+        public string BottomRight { get; set; }
+
+        public State()
+        {
+            this.TurnNumber = 0;
+            this.Character = "X";
+
+            this.TopLeft = "";
+            this.TopMiddle = "";
+            this.TopRight = "";
+
+            this.CenterLeft = "";
+            this.CenterMiddle = "";
+            this.CenterRight = "";
+
+            this.BottomLeft = "";
+            this.BottomMiddle = "";
+            this.BottomRight = "";
+        }
+    }
+
+}
